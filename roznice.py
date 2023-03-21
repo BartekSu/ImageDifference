a4deb1755bc5e8a28cb28641c21c2d06ccbaf503
BartekSu <125037515+BartekSu@users.noreply.github.com>
2023-03-21 12:24:14 +0100

    Update roznice.py

--- roznice.py
+++ roznice.py
@@ -130,37 +130,7 @@ class roznice(wnd, cls):
         pixmap_3 = QPixmap('Oryginal.jpg')
         self.label.setPixmap(pixmap_3)
         self.label_3.setText(str(round(score, 2)))
-        '''
-    #def on_pushButton_4_pressed(self):
-        imageA = plt.imread('las1.jpg')
 
-        imageB = plt.imread('las2.jpg')
-        
-        # convert the images to grayscale
-        grayA = cv2.cvtColor(self.imageA, cv2.COLOR_BGR2GRAY)
-        grayB = cv2.cvtColor(self.imageB, cv2.COLOR_BGR2GRAY)
-
-        # compute the Structural Similarity Index (SSIM) between the two
-        # images, ensuring that the difference image is returned
-        (score, diff) = compare_ssim(grayA, grayB, full=True)
-        diff = (diff * 255).astype("uint8")
-        print("SSIM: {}".format(score))
-
-        # threshold the difference image, followed by finding contours to
-        # obtain the regions of the two input images that differ
-        thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        
-        # loop over the contours
-        for c in cnts:
-            # compute the bounding box of the contour and then draw the
-            # bounding box on both input images to represent where the two
-            # images differ
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(self.imageA, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.rectangle(self.imageB, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        '''
         imageB = cv2.cvtColor(self.imageB, cv2.COLOR_RGB2BGR)
         cv2.imwrite('Modified.jpg', imageB)
 
